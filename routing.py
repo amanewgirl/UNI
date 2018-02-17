1c735b9177817f1a511a2e3b20bee153cc183209
amanewgirl <amanewgirl@users.noreply.github.com>
2018-02-17 12:53:40 +0000

    Update routing.py

--- routing.py
+++ routing.py
@@ -1,21 +1,7 @@
-import os
-from flask import Flask, redirect, url_for, abort,render_template, send_from_directory, request
-from flask_socketio import SocketIO, send
-
+from flask import Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
-
-@socketio.on('message')
-def handleMessage(msg):
-  print('Message: ' + msg)
-  send(msg, broadcast=True)
-
-#this is the homepage of the app
-@app.route("/", methods = ['POST', 'GET'])
-def root():
-    return render_template ('index.html')
-
-if __name__ =="__main__":
-     app.run(host='0.0.0.0')
-     socketio.run(app, host='0.0.0.0')
+@app.route('/')
+def index():
+	return 'Yo, it's working!'
+if __name__ == "__main__":
+	app.run()
