56da18914feb7aedc2a9ccf84f9c057107acb73d
amanewgirl <amantlecraviolatti@gmail.com>
2018-02-22 05:40:34 +0000

    added form

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'Yo, it is working!'
+
+@app.route('/form')
+def form():
+	return render_template ('argumentform.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
