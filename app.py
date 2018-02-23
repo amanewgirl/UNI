baa2a37eafe339b668bda5d8180d8c2386bce6c2
amanewgirl <amantlecraviolatti@gmail.com>
2018-02-23 14:43:43 +0000

    added test image and shuffle game

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def index():
 def form():
 	return render_template ('argumentform.html')
 
+@app.route('/shuffle')
+def puzzle():
+	return render_template ('puzzle.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
