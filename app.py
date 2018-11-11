7510607914c0679ba19d390ea95d68fa0c054296
amanewgirl <amanewgirl@users.noreply.github.com>
2018-11-11 23:14:27 +0000

    Revert "added 2 player"
    This reverts commit bfa13fa7094fabfe97f0cd1b991c988f6d5915e3.

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
-import os
-from flask import Flask, render_template, request, url_for, abort,render_template, send_from_directory
+from flask import Flask, render_template, request
 from werkzeug import secure_filename
-from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
+@app.route('/')
+def index():
+	return 'Yo, it is working!'
 
-number=0
+@app.route('/form')
+def form():
+	return render_template ('workingargumentform.html')
 
-#socketio code to handle messages
-@socketio.on('my event')
-def tryn(int):
-   global number
-   print(number)
-   number=number+1
-   emit('my response',number)
-   return number
-   
-
-#socketio code to handle messages
-@socketio.on('message')
-def stuff(msg):
-    print('Message:' +msg)
-    send(msg, broadcast=True)
+@app.route('/formtest')
+def formtest():
+	return render_template ('argumentformtest.html')
 
 
+@app.route('/data', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['blob']
+      f.save('static/upload.jpg')
+      return 'file uploaded successfully'
 
-#debate game instructions
-@app.route('/instruct')
-def instructions():
-	return render_template('debateInstructions.html')
+@app.route('/shuffle')
+def puz():
+	return render_template ('argSign.html')
 
-#debate game
+@app.route('/shuffle<stage>')
+def puzzle(stage):
+	if stage == '2':
+		return render_template ('argAnalogy.html')
+	elif stage == '3':
+		return render_template ('argExpert.html')
+
+
 @app.route('/debate')
-@app.route('/debate/<player>')
-def debate(player=None):
-	return render_template ('boardFinal.html', player=player)
-
-@app.route('/debatetest')
-def deb():
+def debate():
 	return render_template ('lvl 2Gameboardtest.html')
 
 #if __name__ == "__main__":
