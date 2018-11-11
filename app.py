79e17e984154ce8260d5035d599500f01402a5c4
amanewgirl <amanewgirl@users.noreply.github.com>
2018-11-11 23:43:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def puzzle(stage):
 def debate():
 	return render_template ('lvl 2Gameboardtest.html')
 
-#if __name__ == "__main__":
-#	app.run(host='0.0.0.0')
-
-port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0')
+
+#port = int(os.environ.get("PORT", 5000))
+#if __name__ == "__main__":
+#	app.run(host='0.0.0.0', port=port)
