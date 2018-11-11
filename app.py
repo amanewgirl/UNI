8f4cb87848cf6a5f1662e2d0d7fca63196ef3dc7
amanewgirl <amantlecraviolatti@gmail.com>
2018-11-11 18:52:34 +0000

    launch port isnt hardcoded anymore

--- app.py
+++ app.py
@@ -42,5 +42,9 @@ def debate(player=None):
 def deb():
 	return render_template ('lvl 2Gameboardtest.html')
 
+#if __name__ == "__main__":
+#	app.run(host='0.0.0.0')
+
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0', port=port)
