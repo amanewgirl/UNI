36a0ff5fa43b9f046360441adcf34b4577e638df
amanewgirl <amanewgirl@users.noreply.github.com>
2018-11-13 20:52:01 +0000

    multiplayer + the rest of the site

--- app.py
+++ app.py
@@ -25,7 +25,36 @@ def stuff(msg):
     print('Message:' +msg)
     send(msg, broadcast=True)
 
+@app.route('/')
+def index():
+	return 'Yo, it is working!'
 
+@app.route('/form')
+def form():
+	return render_template ('workingargumentform.html')
+
+@app.route('/formtest')
+def formtest():
+	return render_template ('argumentformtest.html')
+
+
+@app.route('/data', methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['blob']
+      f.save('static/upload.jpg')
+      return 'file uploaded successfully'
+
+@app.route('/shuffle')
+def puz():
+	return render_template ('argSign.html')
+
+@app.route('/shuffle<stage>')
+def puzzle(stage):
+	if stage == '2':
+		return render_template ('argAnalogy.html')
+	elif stage == '3':
+		return render_template ('argExpert.html')
 
 #debate game instructions
 @app.route('/instruct')
