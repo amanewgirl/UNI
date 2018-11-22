eaf36c8067e84331ac986c4ff7c7bcd96f3df2ec
amanewgirl <amantlecraviolatti@gmail.com>
2018-11-22 10:18:27 +0000

    testing multiplayer alone

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, render_template, request, url_for, abort,render_template, send_from_directory
-from werkzeug import secure_filename
+from flask import Flask, redirect, url_for, abort,render_template, send_from_directory, request
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
@@ -15,7 +14,7 @@ def tryn(int):
    global number
    print(number)
    number=number+1
-   emit('my response',number)
+   emit('my response', number)
    return number
    
 
@@ -25,41 +24,17 @@ def stuff(msg):
     print('Message:' +msg)
     send(msg, broadcast=True)
 
-@app.route('/')
+
+
+#Chat
+@app.route("/")
 def index():
-	return 'Yo, it is working!'
-
-@app.route('/form')
-def form():
-	return render_template ('workingargumentform.html')
-
-@app.route('/formtest')
-def formtest():
-	return render_template ('argumentformtest.html')
-
-
-@app.route('/data', methods=['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['blob']
-      f.save('static/upload.jpg')
-      return 'file uploaded successfully'
-
-@app.route('/shuffle')
-def puz():
-	return render_template ('argSign.html')
-
-@app.route('/shuffle<stage>')
-def puzzle(stage):
-	if stage == '2':
-		return render_template ('argAnalogy.html')
-	elif stage == '3':
-		return render_template ('argExpert.html')
-
-#debate game instructions
-@app.route('/instruct')
-def instructions():
 	return render_template('debateInstructions.html')
+
+#@app.route('/debate')
+#def debate():
+#	return render_template ('boardFinal.html') 
+
 
 #debate game
 @app.route('/debate')
@@ -67,14 +42,12 @@ def instructions():
 def debate(player=None):
 	return render_template ('boardFinalTest.html', player=player)
 
-@app.route('/debatetest')
-def deb():
-	return render_template ('lvl 2Gameboardtest.html')
+
+
+
+
+
 
 if __name__ =="__main__":
      app.run(host='0.0.0.0')
      socketio.run(app, host='0.0.0.0')
-
-#port = int(os.environ.get("PORT", 5000))
-#if __name__ == "__main__":
-#	app.run(host='0.0.0.0', port=port)
